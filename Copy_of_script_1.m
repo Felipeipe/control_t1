12d215ecc4554689b049124043877315a4db959e
pipo's PC . <philipejoos@gmail.com>
2025-05-22 14:47:38 -0400

    prueba diseño controladores

---
+++ Copy_of_script_1.m
@@ -0,0 +1,24 @@
+L_fc = 2;
+R_fc = 0.7;
+L_b = 3*10^-3;
+R_b = 0.3;
+C_L = 4000*10^-6;
+R_L = 10;
+
+Z_fc = tf([L_fc R_fc],1);
+Z_b = tf([L_b R_b],1);
+Z_L = tf(C_L,[1 C_L/R_L]);
+
+%%
+den_b = Z_b + Z_L;
+sys_b = 1/den_b;
+rltool(sys_b)
+
+%%
+den_fc = Z_fc + Z_L;
+sys_fc = 1/den_fc;
+rltool(sys_fc)
+
+%%
+sys_VL = Z_L;
+rltool(sys_VL)
