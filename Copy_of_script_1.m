3694e0e1f8664fe8bb1bfc408c1fdb2a5e21f480
Phillipe Joos <phillipe.joos@ug.uchile.cl>
2025-05-22 21:44:45 -0400

    Borrador para simulink

--- Copy_of_script_1.m
+++ Copy_of_script_1.m
@@ -9,16 +9,16 @@ Z_fc = tf([L_fc R_fc],1);
 Z_b = tf([L_b R_b],1);
 Z_L = tf(C_L,[1 C_L/R_L]);
 
-%%
+%% Planta batería
 den_b = Z_b + Z_L;
 sys_b = 1/den_b;
 rltool(sys_b)
 
-%%
+%% Planta Full Celd
 den_fc = Z_fc + Z_L;
 sys_fc = 1/den_fc;
 rltool(sys_fc)
 
-%%
+%% Planta Carga
 sys_VL = Z_L;
 rltool(sys_VL)
